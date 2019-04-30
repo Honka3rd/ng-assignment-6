05430d479185b2300318353542ab83e463a901b3
wangzeli <wangzeli@nobook.com>
2019-04-30 13:01:21 +0800

    6
    Signed-off-by: wangzeli <wangzeli@nobook.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  selectedId;
+  inputSelected:boolean;
+  selectedType;
+
+  constructor(){
+    this.selectedId = -1;
+    this.inputSelected = false;
+    this.selectedType="";
+  }
+
+  courseTypes=[
+    {id:0,name:'Dev'},
+    {id:1,name:'BI'},
+    {id:2,name:'BA'},
+    {id:3,name:'DA'}
+  ];
+
+  submit(f){
+    console.log(f);
+  }
+
+  showCourseName(c){
+    console.log(c);
+  }
+
+  toggleSelect(event,ngModel){
+    event.stopPropagation();
+    this.inputSelected = !this.inputSelected;
+    console.log(ngModel);
+  }
+
+  clickItem(ct){
+    this.selectedType=ct;
+    this.inputSelected = false;
+  }
+
+  clickBlank(){
+    this.inputSelected = false;
+  }
 }
